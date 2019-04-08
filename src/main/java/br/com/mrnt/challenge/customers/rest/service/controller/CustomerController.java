a0413336f6a9920d7874fb01825771d3a54e2d3b
Lucas de Albuquerque <lks.albq@gmail.com>
2019-04-08 11:44:55 -0300

    creating default pagination

--- src/main/java/br/com/mrnt/challenge/customers/rest/service/controller/CustomerController.java
+++ src/main/java/br/com/mrnt/challenge/customers/rest/service/controller/CustomerController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -46,8 +48,9 @@ public class CustomerController {
 	
 	
 
+	
 	@GetMapping("/api/customers")
-	public Page<Customer> retrieveAllCustomers(Pageable pageable) {
+	public Page<Customer> retrieveAllCustomers(@PageableDefault(value=5,size=5, page=0) Pageable pageable) {
 		return customerRepository.findAll(pageable);
 	}
 	
